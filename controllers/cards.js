e4fedb1704c87bd9245be2e88df10e9aa896e61e
srvds8080 <srvds@yandex.ru>
2020-12-12 23:00:00 +0300

    feat: todo callBacks for routes

---
+++ controllers/cards.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
+const getDataFromFile = require('../helpers/files');
+
+const getAllCards = (req, res) => {
+  return getDataFromFile(dataPath)
+    .then((cards) => res.status(200).send(cards))
+    .catch((error) => res.status(500).send(error))
+}
+
+module.exports = getAllCards;
---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const dataPath = path.join(__dirname, '..', 'data', 'users.json');
+const getDataFromFile = require('../helpers/files');
+
+const getAllUsers = (req, res) => {
+  return getDataFromFile(dataPath)
+    .then((users) => res.status(200).send(users))
+    .catch((error) => res.status(500).send(error))
+}
+
+const getUser = (req, res) => {
+  return getDataFromFile(dataPath)
+    .then((users) => users.find(user => user._id === req.params.id))
+    .then(user => {
+
+      if (!user) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      return res.status(200).send(user)
+    })
+    .catch(e => res.status(500).send({ error: `somthing went wrong, error: ${e}` }))
+}
+
+
+module.exports = { getAllUsers, getUser };
