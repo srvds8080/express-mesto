b47d3d110334fab12ef3abff93f8663122b4c29b
srvds8080 <srvds@yandex.ru>
2020-12-13 13:46:44 +0300

    bagFix: fix response status 404 && remove require "http-errors"

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const createError = require('http-errors');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 
@@ -11,6 +10,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use((req, res) => {
-  res.send(createError(404, 'Запрашиваемый ресурс не найден'));
+  res
+    .status(404)
+    .send({ message: 'Запрашиваемый ресурс не найден' });
 });
 app.listen(PORT);
