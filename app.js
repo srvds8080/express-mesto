daa68eb11b989fe7a2a4813619f8a39c6a1358b3
srvds8080 <srvds@yandex.ru>
2020-12-27 18:28:33 +0300

    feat: подеключил приложение к mongo

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users.js');
 const cardsRouter = require('./routes/cards.js');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb+srv://srvds:1234qwer@cluster0.vzqr2.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5fe64d1aef5394976c2707b0',
+  };
+  next();
+});
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
 app.use((req, res) => {
