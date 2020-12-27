e0ca80f9a590fc545fb0779eaca3c42c7b69338d
srvds8080 <srvds@yandex.ru>
2020-12-27 18:29:33 +0300

    feat: удалить файлы имитирующие БД

--- helpers/files.js
+++
@@ -1,8 +0,0 @@
-const fsPromises = require('fs').promises;
-
-const getDataFromFile = (pathToFile) => fsPromises.readFile(pathToFile, { encoding: 'utf-8' })
-  .then((data) => JSON.parse(data))
-  // eslint-disable-next-line no-console
-  .catch((error) => console.error(error));
-
-module.exports = getDataFromFile;
